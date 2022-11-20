4176f7b798d2303d1d412e86627114bb3843c507
KMukhs <118463397+K-Mukhs@users.noreply.github.com>
2022-11-20 09:34:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Band name generator program: it asks for a city that you grew up in and the name of your pet and then it combines those together and gives you the name of your band.
+
+print("Welcome to the Band generator!")
+city = input("Which city do yo grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your brand's name could be: " + city + " " + pet)
